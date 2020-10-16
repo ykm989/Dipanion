a4afb1328d1a0348884e469af7fedddec797aaaa
ykm989 <ykm989@naver.com>
2020-10-16 16:56:14 +0900

    move
    대충 완성

--- Assets/Scenes/ChaT/AutoMove.cs
+++ Assets/Scenes/ChaT/AutoMove.cs
@@ -6,57 +6,60 @@ public class AutoMove : MonoBehaviour
 {
     public float xmoved = 0f;//x축 이동방향
     public float ymoved = 0f;//y축 이동방향
-    public float speed = 10f;//이동속도
+    public float speed = 0.000000001f;//이동속도
     private Vector3 direction = Vector3.zero;
     void Start()
     {
-        //xmoved = Random.Range(-1.0f, 1.0f);
-        //ymoved = Random.Range(-1.0f, 1.0f);
+        move_direction();
 
     }
 
 
-    void move()
+    void move_direction()
     {
-        Vector3 direction = Vector3.zero;
-        direction.x = xmoved;
-        direction.y = ymoved;
-
+        direction.x = Random.Range(-1.0f, 1.0f);
+        direction.y = Random.Range(-1.0f, 1.0f);
     }
     // Update is called once per frame
     void Update()
     {
         Move();
+        //move_direction();
         outcheck();
     }
     void outcheck()//화면 밖 나갔는지 체크
     {
-        Vector3 vp = Camera.main.WorldToViewportPoint(transform.position);
-        int rr = 0;
+        Vector3 vp = Camera.main.WorldToViewportPoint(this.transform.position);
         if (vp.x < 0f)//왼쪽 화면 밖
         {
-            xmoved *= -1f;
-            ymoved = Random.Range(-1.0f, 1.0f);
+            Debug.Log(1);
+            direction.x *= -1f;
+            direction.y = Random.Range(-1.0f, 1.0f);
         }
         else if (vp.x > 1f)//우측 화면 밖
         {
-            xmoved *= -1f;
-            ymoved = Random.Range(-1.0f, 1.0f);
+            Debug.Log(2);
+            direction.x *= -1f;
+            direction.y = Random.Range(-1.0f, 1.0f);
         }
         else if (vp.y < 0f)//하단 화면 밖
         {
-            xmoved = Random.Range(-1.0f, 1.0f);
-            ymoved *= -1f;
+            Debug.Log(3);
+            direction.x = Random.Range(-1.0f, 1.0f);
+            direction.y *= -1f;
         }
         else if (vp.y > 1f)
         {
-            xmoved = Random.Range(-1.0f, 1.0f);
-            ymoved *= -1f;
+            Debug.Log(4);
+            direction.x = Random.Range(-1.0f, 1.0f);
+            direction.y *= -1f;
         }
     }
     void Move()//이동 함수
     {
-        transform.position += direction * speed * Time.deltaTime;
-        //transform.Translate(direction * speed * Time.deltaTime,Space.World);
+        //transform.position = direction;
+        
+        this.transform.Translate(direction  * Time.deltaTime * 3);
+        Debug.Log(direction);
     }
 }
